c5ea7563e766e44333c9f69ccbef208333d023b0
VinceEmond <vince.emond@icloud.com>
2022-03-02 21:40:55 +0000

    Implemented an override to the form submit via a submit listener. Serialized input text

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -107,6 +107,32 @@ $(() => {
     }
   };
 
+
+
+
+  $('.new-tweet-form').submit(function(event) {
+    event.preventDefault();
+    // console.log("You pressed the submit button!");
+    // console.log("this value", this.text.value);
+    const $serializedText = $(this).serialize();
+    
+
+    
+    // $.ajax({
+    //   url: '/tweets',
+    //   method: "POST"
+    // })
+    //   .then(function(data) {
+    //     console.log("data", data);
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Error:", error);
+
+    //   });
+
+    
+  });
+
   renderTweets(data);
 
 });
