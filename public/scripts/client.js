e7fea19cfafa30391d74a6175873f656583b94e1
VinceEmond <vince.emond@icloud.com>
2022-03-02 17:12:27 +0000

    Reduced margin for the tweet text to better accomodate texts with text up to 140 characters

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,7 +18,8 @@ $(() => {
         "handle": "@SirIsaac"
       },
       "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+        // "text": "If I have seen further it is by standing on the shoulders of giants"
+        "text": "Bacon ipsum dolor amet short ribs landjaeger drumstick ground round t-bone tongue cow chuck corned beef short loin meatloaf swine tenderloi."
       },
       "created_at": 1645982106211
     },
@@ -39,11 +40,12 @@ $(() => {
   // console.log("tweetsDb.user", tweetsDb.user);
 
   const createTweetElement = function(tweetObj) {
+    const {name, avatars, handle} = tweetObj.user;
+    const {text} = tweetObj.content;
+    const timestampMs = tweetObj["created_at"];
+    const timeInDays = Math.round((Date.now() - timestampMs) / 86400000);
 
-    const {name, avatars,handle} = tweetObj[0].user;
-    
 
-    // const name = "Vince Emond";
     const $tweet = $(`
     <article class="tweet">
   
@@ -59,12 +61,12 @@ $(() => {
           </header>
   
           <div class="middle">
-            <p> Hello world!</p>
+            <p>${text}</p>
           </div>
   
           <footer>
             <div class="tweet-footer-left">
-              <p>10 days ago</p>
+              <p>${timeInDays} days ago</p>
             </div>
             <div class="tweet-footer-right">
               <p><i class="fa-solid fa-flag"></i></p>
@@ -80,6 +82,6 @@ $(() => {
     return $tweet;
   };
 
-  $('#tweets-container').prepend(createTweetElement(tweetsDb));
+  $('#tweets-container').prepend(createTweetElement(tweetsDb[0]));
 
 });
