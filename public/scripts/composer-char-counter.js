0ab3eeab6accad13df246c4aa6235c2e6f2f76f3
VinceEmond <vince.emond@icloud.com>
2022-03-04 08:06:17 +0000

    Cleaned up some code, refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,14 @@
 
 
+/******************/
+/* AFTER PAGE LOADS
+/*****************/
 $(() => {
-
   const $textArea = $('.new-tweet-textbox');
   
-  let charCount = 0;
-
   $textArea.on('input', function() {
+    let charCount = 0;
     charCount = 140 - this.value.length;
-    
-    // ********************************
-    // THREE WAYS TO DO THE SAME THING
-    // ********************************
-    //$('.new-tweet-lowerhalf-counter').text(charCount);
-    // const $counter = $(this).parent().siblings().first().children().last();
     const $counter = $(this).parents().find('.new-tweet-lowerhalf-counter');
 
     charCount < 0 ? $counter.addClass('addRed') : $counter.removeClass('addRed');
