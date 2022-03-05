5f67c0aee96b79fa65b4b7e783c98388055443a1
VinceEmond <vince.emond@icloud.com>
2022-03-05 00:44:36 +0000

    Cleaned up functions and created variables for jquery elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,31 @@
 
-/******************/
-/* AFTER PAGE LOADS
-/*****************/
+/****************************/
+/*     AFTER PAGE LOADS     */
+/****************************/
 $(() => {
 
-  const escape = function(str) {
+
+  /****************************/
+  /*        VARIABLES         */
+  /****************************/
+  const $window = $(window);
+  const $newTweetWrapper = $(".new-tweet-wrapper");
+  const $newTweetTextBox = $(".new-tweet-textbox");
+  const $newTweetForm = $('.new-tweet-form');
+  const $validationError = $('.validation-error');
+  const $backToTopButton = $(".back-to-top-wrapper");
+  const $tweetsContainer = $('#tweets-container');
+  const $createNewTweetButton = $(".nav-bar-right");
+  const $errorMessage = $('.error-message');
+  const $charCounter = $('.new-tweet-lowerhalf-counter');
+
+
+  /****************************/
+  /*        FUNCTIONS         */
+  /****************************/
+  const escape = function(textString) {
     let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
+    div.appendChild(document.createTextNode(textString));
     return div.innerHTML;
   };
 
@@ -54,128 +73,118 @@ $(() => {
     return $tweet;
   };
 
-
-
-  $(window).scroll(function() {
-    scrollFunction();
-  });
-
-  $(".back-to-top-circle").on('click', function() {
-    $(window).scrollTop("top");
-    $(".new-tweet-textbox").focus();
-  });
-
-  const scrollFunction = function() {
-    if ($(window).scrollTop() > 400) {
-      $(".back-to-top-wrapper").show();
-    } else {
-      $(".back-to-top-wrapper").hide();
-    }
+  const toggleBackToTopButton = function() {
+    $window.scrollTop() > 400 ? $backToTopButton.show() : $backToTopButton.hide();
   };
-
 
   const renderTweets = function(tweetsDatabase) {
     for (const tweet of tweetsDatabase) {
-      $('#tweets-container').prepend(createTweetElement(tweet));
+      $tweetsContainer.prepend(createTweetElement(tweet));
     }
   };
 
+  const resetNewTweetForm = function(textArea) {
+    textArea.text.value = "";
+    $charCounter.text("140");
+    $newTweetTextBox.focus();
+  };
 
-  $(".nav-bar-right").on("click", function() {
+  const showValidationError = function(errorMsg) {
+    $errorMessage.text(errorMsg);
+    $validationError.slideDown(400);
+    $newTweetTextBox.focus();
+  };
 
-    //If not a top of page, go to top
-    if ($(window).scrollTop() !== 0) {
-      $(window).scrollTop("top");
-      $('.validation-error').slideUp(400);
-
-
-      // Slide down the new tweet dialog and set focus
-      $(".new-tweet").slideDown("slow").promise().done(() => {
-        $(".new-tweet-textbox").focus();
-        $('.validation-error').slideUp(400);
-      });
-      
-    // If already at top, toggle new tweet section
-    } else {
-      $('.validation-error').slideUp(400);
-      $(".new-tweet").slideToggle("slow").promise().done(() => {
-        $(".new-tweet-textbox").focus();
-      });
-    }
-
-
-  });
- 
-
-  // Setup textbox to submit on enter
-  // Allows for shift-enter to create a new line
-  $(".new-tweet-textbox").keypress(function(e) {
-    if (e.which === 13 && !e.shiftKey) {
-      e.preventDefault();
-      $(this).closest("form").submit();
-    }
-  });
-
-  $('.new-tweet-form').submit(function(event) {
+  $newTweetForm.submit(function(event) {
     event.preventDefault();
 
-    // Slide-up Error message, THEN do the rest
-    $('.validation-error').slideUp(400, () => {
+    // Slide-up error message before attempting to submit
+    $validationError.slideUp(400, () => {
 
       // Catch if new tweet textarea is empty
       if (!this.text.value) {
-        console.log("Error: No text has been input!");
-        // alert("Error: No text has been input!");
-        $('.error-message').text("Error: No text has been input!");
-        $('.validation-error').slideDown(400);
-        $(".new-tweet-textbox").focus();
+        showValidationError("Error: No text has been input!");
         return;
       }
 
       // Catch if new tweet textarea contains too many characters
       if (this.text.value.length > 140) {
-        console.log("Error: You have entered too many characters!");
-        // alert("Error: You have entered too many characters!");
-        $('.error-message').text("Error: You have entered too many characters!");
-        $('.validation-error').slideDown(400);
-        $(".new-tweet-textbox").focus();
+        showValidationError("Error: You have entered too many characters!");
         return;
       }
 
-
       // Process AJAX POST request
       const serializedText = $(this).serialize();
-
       $.ajax({url: '/tweets', method: "POST", data: serializedText})
         .then(() => {
-          // console.log("Tweet AJAX post: sucess!");
-          this.text.value = "";
-          $('.new-tweet-lowerhalf-counter').text("140");
-          $('.new-tweet-textbox').removeClass('addRed');
-          $(".new-tweet-textbox").focus();
+          resetNewTweetForm(this);
           loadTweets();
         })
         .catch((error) => {
           console.log("Error:", error);
         });
-
     });
-
   });
-
 
   const loadTweets = function() {
     $.ajax({url: '/tweets', method: "GET"})
       .then(function(data) {
         renderTweets(data);
-        $(".new-tweet-textbox").focus();
+        $newTweetTextBox.focus();
       })
       .catch((error) => {
         console.log("Error :", error);
       });
   };
 
-  $('.validation-error').hide();
-  loadTweets();
 
+  /****************************/
+  /*      EVENT LISTENERS     */
+  /****************************/
+  $window.scroll(function() {
+    toggleBackToTopButton();
+  });
+
+  $backToTopButton.on('click', function() {
+    $window.scrollTop("top");
+    $newTweetTextBox.focus();
+  });
+
+  // Enable enter key to submit new tweets
+  // (Also allows for shift-enter to create a new line)
+  $newTweetTextBox.keypress(function(e) {
+    const enterKey = 13;
+    if (e.which === enterKey && !e.shiftKey) {
+      e.preventDefault();
+      $newTweetForm.submit();
+    }
+  });
+
+  $createNewTweetButton.on("click", function() {
+    //If not a top of page, go to top
+    if ($window.scrollTop() !== 0) {
+      $window.scrollTop("top");
+      $validationError.slideUp(400);
+
+      // Slide down the new tweet dialog and set focus
+      $newTweetWrapper.slideDown("slow").promise().done(() => {
+        $newTweetTextBox.focus();
+        $validationError.slideUp(400);
+      });
+      
+    // If already at top, toggle new tweet section
+    } else {
+      $validationError.slideUp(400);
+      $newTweetWrapper.slideToggle("slow").promise().done(() => {
+        $newTweetTextBox.focus();
+      });
+    }
+  });
+
+
+  /****************************/
+  /*        INITIAL LOAD      */
+  /****************************/
+  $validationError.hide();
+  loadTweets();
 });
